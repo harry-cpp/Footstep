23ba09bf7c501e16a6a14d548540671d89cb8681
Jean-David Moisan <vynecompiler@gmail.com>
2018-07-28 17:31:44 -0400

    Removed commented out code

--- Footstep/Floor.cs
+++ Footstep/Floor.cs
@@ -20,26 +20,6 @@ namespace Footstep
 
         public Floor()
         {
-            /*_vertices = new VertexPositionNormalTexture[6];
-
-            _vertices[0].Position = new Vector3(-20, -20, 0);
-            _vertices[1].Position = new Vector3(-20, 20, 0);
-            _vertices[2].Position = new Vector3(20, -20, 0);
-
-            _vertices[3].Position = _vertices[1].Position;
-            _vertices[4].Position = new Vector3(20, 20, 0);
-            _vertices[5].Position = _vertices[2].Position;
-
-            int repetition = 40;
-
-            _vertices[0].TextureCoordinate = new Vector2(0, 0);
-            _vertices[1].TextureCoordinate = new Vector2(0, repetition);
-            _vertices[2].TextureCoordinate = new Vector2(repetition, 0);
-
-            _vertices[3].TextureCoordinate = _vertices[1].TextureCoordinate;
-            _vertices[4].TextureCoordinate = new Vector2(repetition, repetition);
-            _vertices[5].TextureCoordinate = _vertices[2].TextureCoordinate;*/
-
             generateFloor();
         }
 
