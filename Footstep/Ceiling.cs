82c0d8daaadd4ad0640cef32175a5c4e4e602a2c
Jean-David Moisan <vynecompiler@gmail.com>
2018-07-27 23:08:23 -0400

    Floor + Ceiling + Wall

--- Footstep/Ceiling.cs
+++ Footstep/Ceiling.cs
@@ -4,37 +4,37 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Footstep
 {
-    class Floor : IGameObject
+    class Ceiling : IGameObject
     {
         private Texture2D _texture;
         private VertexPositionNormalTexture[] _vertices;
 
-        public Floor()
+        public Ceiling()
         {
             _vertices = new VertexPositionNormalTexture[6];
 
-            _vertices[0].Position = new Vector3(-20, -20, 0);
-            _vertices[1].Position = new Vector3(-20, 20, 0);
-            _vertices[2].Position = new Vector3(20, -20, 0);
+            _vertices[0].Position = new Vector3(20, -20, 1);
+            _vertices[1].Position = new Vector3(-20, 20, 1);
+            _vertices[2].Position = new Vector3(-20, -20, 1);
 
-            _vertices[3].Position = _vertices[1].Position;
-            _vertices[4].Position = new Vector3(20, 20, 0);
-            _vertices[5].Position = _vertices[2].Position;
+            _vertices[3].Position = _vertices[0].Position;
+            _vertices[4].Position = new Vector3(20, 20, 1);
+            _vertices[5].Position = _vertices[1].Position;
 
             int repetition = 40;
 
-            _vertices[0].TextureCoordinate = new Vector2(0, 0);
+            _vertices[0].TextureCoordinate = new Vector2(repetition, 0);
             _vertices[1].TextureCoordinate = new Vector2(0, repetition);
-            _vertices[2].TextureCoordinate = new Vector2(repetition, 0);
+            _vertices[2].TextureCoordinate = new Vector2(0, 0);
 
-            _vertices[3].TextureCoordinate = _vertices[1].TextureCoordinate;
+            _vertices[3].TextureCoordinate = _vertices[0].TextureCoordinate;
             _vertices[4].TextureCoordinate = new Vector2(repetition, repetition);
-            _vertices[5].TextureCoordinate = _vertices[2].TextureCoordinate;
+            _vertices[5].TextureCoordinate = _vertices[1].TextureCoordinate;
         }
 
         public override void Load(ContentManager content, GraphicsDeviceManager graphics)
         {
-            _texture = content.Load<Texture2D>("Floor");
+            _texture = content.Load<Texture2D>("Ceiling");
         }
 
         public override void Draw(GameTime gameTime, Camera camera)
--- Footstep/Wall.cs
+++ Footstep/Wall.cs
@@ -4,37 +4,35 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Footstep
 {
-    class Floor : IGameObject
+    class Wall : IGameObject
     {
         private Texture2D _texture;
         private VertexPositionNormalTexture[] _vertices;
 
-        public Floor()
+        public Wall()
         {
             _vertices = new VertexPositionNormalTexture[6];
 
             _vertices[0].Position = new Vector3(-20, -20, 0);
-            _vertices[1].Position = new Vector3(-20, 20, 0);
-            _vertices[2].Position = new Vector3(20, -20, 0);
+            _vertices[1].Position = new Vector3(20, -20, 0);
+            _vertices[2].Position = new Vector3(-20, -20, 1);
 
             _vertices[3].Position = _vertices[1].Position;
-            _vertices[4].Position = new Vector3(20, 20, 0);
+            _vertices[4].Position = new Vector3(20, -20, 1);
             _vertices[5].Position = _vertices[2].Position;
 
-            int repetition = 40;
-
             _vertices[0].TextureCoordinate = new Vector2(0, 0);
-            _vertices[1].TextureCoordinate = new Vector2(0, repetition);
-            _vertices[2].TextureCoordinate = new Vector2(repetition, 0);
+            _vertices[1].TextureCoordinate = new Vector2(40, 0);
+            _vertices[2].TextureCoordinate = new Vector2(0, 1);
 
             _vertices[3].TextureCoordinate = _vertices[1].TextureCoordinate;
-            _vertices[4].TextureCoordinate = new Vector2(repetition, repetition);
+            _vertices[4].TextureCoordinate = new Vector2(40, 1);
             _vertices[5].TextureCoordinate = _vertices[2].TextureCoordinate;
         }
 
         public override void Load(ContentManager content, GraphicsDeviceManager graphics)
         {
-            _texture = content.Load<Texture2D>("Floor");
+            _texture = content.Load<Texture2D>("Wall");
         }
 
         public override void Draw(GameTime gameTime, Camera camera)
